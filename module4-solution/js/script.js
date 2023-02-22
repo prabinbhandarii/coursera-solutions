23dabbe8e1f3e30a9c99f98f8f000806b84c23ff
Mr.Anon <101789762+prabinbhandarii@users.noreply.github.com>
2023-02-22 11:35:12 +0545

    Delete module4-solution directory

--- module4-solution/js/script.js
+++
@@ -1,9 +0,0 @@
-let names = ["prabin", "john", "junkiri", "ram", "Juliet", "Jyoti", "samikshya", "pooja", "barsha", "Jamuna"];
-for(var i = 0; i < names.length; i++){
-    if(names[i].charAt(0) === "J" || names[i].charAt(0) === "j"){
-        console.log("Goodbye " + names[i]);
-    }
-    else{
-        console.log("Hello " + names[i]);
-    }
-}
